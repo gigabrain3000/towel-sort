4d0895f94e6b0dc4ac96c8af0027dc6b32b1165c
gigabrain3000 <vladislav.ivanchikov15@gmail.com>
2024-08-08 23:01:18 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix !== undefined ? matrix.map((arr) => matrix.indexOf(arr) % 2 ? arr.reverse() : arr).flat() : [];
 }
